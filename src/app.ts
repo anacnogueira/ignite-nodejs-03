6ab5e1b9101eeb9c87f42e81936a72633d51db91
Ana Claudia <anacnogueira@gmail.com>
2023-06-26 18:43:35 -0300

    Instalando Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 export const app = fastify();
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Ana Claudia",
+    email: "anacnogueira@gmail,com",
+  },
+});
