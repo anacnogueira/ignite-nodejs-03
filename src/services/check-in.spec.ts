f2512627489d1f9d44629279d5273bf19f0cbbee
Ana Claudia <anacnogueira@gmail.com>
2023-07-11 17:17:16 -0300

    Validando dados do check-in

--- src/services/check-in.spec.ts
+++ src/services/check-in.spec.ts
@@ -1,15 +1,28 @@
 import { expect, describe, it, beforeEach, vi, afterEach } from "vitest";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
 import { CheckInService } from "./check-in";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { Decimal } from "@prisma/client/runtime/library";
 
 let checkInsRepository: InMemoryCheckInsRepository;
+let gymsRepository: InMemoryGymsRepository;
 let sut: CheckInService;
 
 describe("Check-in Service", () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository();
-    sut = new CheckInService(checkInsRepository);
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CheckInService(checkInsRepository, gymsRepository);
     vi.useFakeTimers();
+
+    gymsRepository.items.push({
+      id: "gym-01",
+      title: "Academia da Ana",
+      description: "",
+      phone: "",
+      latitude: new Decimal(-23.3005716),
+      longitude: new Decimal(-45.9515282),
+    });
   });
 
   afterEach(() => {
@@ -17,10 +30,9 @@ describe("Check-in Service", () => {
   });
 
   it("should be able to check in", async () => {
-    vi.setSystemTime(new Date(2023, 6, 11, 8, 0, 0));
     const { checkIn } = await sut.execute({
-      gymId: "gym-01",
       userId: "user-01",
+      gymId: "gym-01",
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
@@ -30,14 +42,18 @@ describe("Check-in Service", () => {
     vi.setSystemTime(new Date(2023, 6, 11, 8, 0, 0));
 
     await sut.execute({
-      gymId: "gym-01",
       userId: "user-01",
+      gymId: "gym-01",
+      userLatitude: 0,
+      userLongitude: 0,
     });
 
     await expect(() =>
       sut.execute({
         gymId: "gym-01",
         userId: "user-01",
+        userLatitude: 0,
+        userLongitude: 0,
       })
     ).rejects.toBeInstanceOf(Error);
   });
@@ -46,8 +62,10 @@ describe("Check-in Service", () => {
     vi.setSystemTime(new Date(2023, 6, 11, 8, 0, 0));
 
     await sut.execute({
-      gymId: "gym-01",
       userId: "user-01",
+      gymId: "gym-01",
+      userLatitude: 0,
+      userLongitude: 0,
     });
 
     vi.setSystemTime(new Date(2023, 6, 12, 8, 0, 0));
