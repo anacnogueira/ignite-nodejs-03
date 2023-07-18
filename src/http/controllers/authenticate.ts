a57556ac7124a3d813f6d943b2dc6186b95fae21
Ana Claudia <anacnogueira@gmail.com>
2023-07-18 18:17:42 -0300

    Implementando JEWT no fastify

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -17,9 +17,22 @@ export async function authenticate(
   try {
     const registerService = makeAuthenticateService();
 
-    await registerService.execute({
+    const { user } = await registerService.execute({
       email,
       password,
+    });
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      }
+    );
+
+    return reply.status(200).send({
+      token,
     });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
@@ -28,6 +41,4 @@ export async function authenticate(
 
     throw error;
   }
-
-  return reply.status(200).send();
 }
