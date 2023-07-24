ef35db60015a190abb2d364b47ac42bbc3eef5c3
Ana Claudia <anacnogueira@gmail.com>
2023-07-24 19:43:11 -0300

    Teste E2E da rotas de check-ins

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,37 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+import { prisma } from "@/lib/prisma";
+
+describe("Create Check-in (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to create a check-in", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: "Academia da Ana",
+        latitude: -23.2925701,
+        longitude: -45.94315,
+      },
+    });
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        latitude: -23.2925701,
+        longitude: -45.94315,
+      });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
