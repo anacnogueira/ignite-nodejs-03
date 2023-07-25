ee2070de2658419d4397542cad48a56d3372c016
Ana Claudia <anacnogueira@gmail.com>
2023-07-25 20:36:36 -0300

    Adicionando Midlewares de permissão por role

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -13,5 +13,9 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.get("/check-ins/metrics", metrics);
 
   app.post("/gyms/:gymId/check-ins", create);
-  app.patch("/check-ins/:checkInId/validate", validate);
+  app.patch(
+    "/check-ins/:checkInId/validate",
+    { onRequest: [verifyUserRole("ADMIN")] },
+    validate
+  );
 }
